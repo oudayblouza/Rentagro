39d489b0f01952be50f20e4d92e09ce304615559
Ouaz Seddik <ouazseddik@MacBook-Pro-de-Ouaz.local>
2017-11-07 11:23:54 +0100

    Initial Commit

---
+++ RentAGROTests/RentAGROTests.swift
@@ -0,0 +1,36 @@
+//
+//  RentAGROTests.swift
+//  RentAGROTests
+//
+//  Created by Ouaz Seddik on 07/11/2017.
+//  Copyright © 2017 Ouaz Seddik. All rights reserved.
+//
+
+import XCTest
+@testable import RentAGRO
+
+class RentAGROTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
